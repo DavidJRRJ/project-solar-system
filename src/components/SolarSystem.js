fa7f2a6f2a5a0746d93523dfe29e6c8c7dcf6857
David Junior <david.lourenco94@gmail.com>
2022-06-21 18:40:37 -0300

    Sexto Requisito

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    console.log(planets);
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          {planets.map(({ name, image }, e) => (
+            <li key={ e }>
+              <PlanetCard
+                planetName={ name }
+                planetImage={ image }
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
