9d984c77e7db858698096afcfe2323dae7a503d0
David Junior <david.lourenco94@gmail.com>
2022-06-21 20:44:57 -0300

    Último Requisito

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
+    console.log(missions);
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <ul>
+          {missions.map((e) => (
+            <li key={ e }>
+              <MissionCard
+                name={ e.name }
+                year={ e.year }
+                country={ e.country }
+                destination={ e.destination }
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
